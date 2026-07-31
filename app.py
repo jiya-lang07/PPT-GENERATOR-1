32321335a613e40f311dac14a942f14055528a30
jiya-lang07 <j6372940@gmail.com>
2026-07-31 13:16:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,11 +129,14 @@ def run_agent(leader_agent, query):
 
 #============== STEP 7 AGENT CALL==================
 # leader_agent creation
+if all(ALL_API):
 leader_agent = create_agent(
     model = model,
     tools = [search_latest_info,
              generate_image]
 )
+else:
+  st.info("Pass-All-APP-KEYS and Rerun")
 
 #=================STEP 8 NAVBAR STREAMLIT================
 tab1,tab2,tab3 = st.tabs(["Geneate Image",
